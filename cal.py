cd36058404afcea16163231c8e3e8217d6148a8d
Rishik1507 <microsoftrishik@gmail.com>
2024-11-07 21:40:20 +0530

    c

---
+++ cal.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+x=int(input('Enter the first Number: '))
+y=int(input("Enter the second Number: "))
+print("Adddition: ",add(x,y))
+print("Subtraction ",sub(x,y))
+print("Multiplication: ",mul(x,y))
+print("Division: ",div(x,y))
